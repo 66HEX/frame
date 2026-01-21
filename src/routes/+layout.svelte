3ad01eb6df7e623a59bfc10c4e76cd19fb799f53
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2026-01-21 23:00:34 +0100

    feat: conditionally apply rounded corners on macOS only

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,19 @@
-<script>
+<script lang="ts">
 	let { children } = $props();
+	import { onMount } from 'svelte';
 	import './layout.css';
+	import { type } from '@tauri-apps/plugin-os';
+
+	let platform = $state<string | null>(null);
+
+	onMount(() => {
+		platform = type();
+	});
 </script>
 
 <div
-	class="**:focus:ring- -mt-2.5 flex h-screen flex-col overflow-hidden rounded-2xl border-none bg-black/60 select-none **:focus:outline-none"
+	class="**:focus:ring-none flex h-screen flex-col overflow-hidden border-none bg-black/60 select-none **:focus:outline-none"
+	class:rounded-2xl={platform === 'macos'}
 >
 	<div class="relative flex-1">
 		{@render children()}
