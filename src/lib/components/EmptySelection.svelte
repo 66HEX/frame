a10be06d794bbb8b6398aacaf4935a8119a8c301
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2026-01-24 12:59:30 +0100

    style: fix card color consistency

--- src/lib/components/EmptySelection.svelte
+++ src/lib/components/EmptySelection.svelte
@@ -1,5 +1,5 @@
 <div
-	class="relative flex h-full flex-col items-center justify-center overflow-hidden bg-ds-gray-100/5 p-8 text-center"
+	class="relative flex h-full flex-col items-center justify-center overflow-hidden p-8 text-center"
 >
 	<p class="text-gray-alpha-600 max-w-50 text-[10px] font-medium">
 		SELECT AN ITEM FROM THE QUEUE TO ACCESS CONFIGURATION
