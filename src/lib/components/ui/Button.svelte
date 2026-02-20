575a6675f1cc4682d12650d8bc0fe17899791f92
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2026-02-21 00:24:00 +0100

    dsf

--- src/lib/components/ui/Button.svelte
+++ src/lib/components/ui/Button.svelte
@@ -4,19 +4,19 @@
 	import type { HTMLButtonAttributes } from 'svelte/elements';
 
 	const buttonVariants = cva(
-		'inline-flex items-center justify-center whitespace-nowrap rounded-sm text-[10px] font-semibold transition-all disabled:pointer-events-none disabled:transition-none shadow-sm shadow-black/5',
+		'inline-flex items-center justify-center whitespace-nowrap rounded-sm text-[10px] font-semibold transition-all disabled:pointer-events-none disabled:transition-none',
 		{
 			variants: {
 				variant: {
 					default:
-						'bg-blue-700 text-foreground button-highlight text-shadow hover:bg-blue-700/90 disabled:text-foreground/50 disabled:bg-blue-700/50 disabled:hover:bg-blue-700/50',
+						'btn-primary disabled:text-foreground/50 disabled:opacity-50',
 					secondary:
-						'bg-gray-alpha-100 button-highlight text-shadow text-foreground hover:bg-gray-alpha-200 disabled:bg-gray-alpha-100 disabled:text-foreground/50 disabled:opacity-50',
+						'btn-secondary disabled:text-foreground/50 disabled:opacity-50',
 					ghost:
 						'hover:bg-gray-alpha-100 text-gray-alpha-600 hover:text-foreground disabled:bg-transparent disabled:opacity-50',
 					'titlebar-ghost': 'text-gray-alpha-600 hover:text-foreground disabled:opacity-50',
 					destructive:
-						'bg-gray-alpha-100 button-highlight text-shadow text-red-700 disabled:opacity-50 hover:bg-gray-alpha-200 disabled:bg-gray-alpha-100 disabled:text-red-700/50 ',
+						'btn-destructive disabled:opacity-50 disabled:text-red-700/60',
 					'titlebar-destructive':
 						'text-gray-alpha-600 hover:bg-red-700 hover:text-foreground disabled:opacity-50'
 				},
